9a6f9553a7230488cf8399df62285278eca6737b
hug0cr <pro.hugocamacho@gmail.com>
2022-01-11 09:42:48 +0100

    Ajout tests JUnit

--- src/test/java/com/hug0cr/SpringSecurityAuthApplicationTests.java
+++ src/test/java/com/hug0cr/SpringSecurityAuthApplicationTests.java
@@ -1,13 +1,25 @@
 package com.hug0cr;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.hug0cr.controller.LoginController;
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class SpringSecurityAuthApplicationTests {
+	
+	@Autowired
+	private LoginController controller;
 
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/hug0cr/SpringWebAppTest.java
@@ -0,0 +1,66 @@
+/**
+ * 
+ */
+package com.hug0cr;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ * @author hug0cr
+ * @date 2022-01-11
+ */
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SpringWebAppTest {
+
+	@Autowired
+	private MockMvc mock;
+
+	@Autowired
+	private WebApplicationContext context;
+
+	@Test
+	public void shouldReturnDefaultMessage() throws Exception {
+		mock.perform(get("/login")).andDo(print()).andExpect(status().isOk());
+	}
+
+	@Test
+	public void userLoginTest() throws Exception {
+		mock.perform(formLogin("/login").user("springuser")
+				.password("spring123")).andExpect(authenticated());
+	}
+	
+	@Test
+	public void userLoginFail() throws Exception {
+		mock.perform(formLogin("/login").user("springuser")
+				.password("password")).andExpect(unauthenticated());
+	}
+
+	@BeforeEach
+	public void setup() {
+		mock = MockMvcBuilders
+				.webAppContextSetup(context)
+				.apply(springSecurity())
+				.build();
+	}
+
+}
